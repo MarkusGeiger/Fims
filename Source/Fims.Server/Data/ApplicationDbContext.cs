d9079e7d29f9b0c0c8872386da905afdc8af8f1b
Markus <5563643+MarkusGeiger@users.noreply.github.com>
2024-05-10 16:48:33 +0200

    Add Identitiy API and move all api mappings behind /api path

---
+++ Source/Fims.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+using System.Net.Mime;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fims.Server.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+  public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+  {
+    
+  }
+
+  protected override void OnModelCreating(ModelBuilder builder)
+  {
+    base.OnModelCreating(builder);
+    // Customization and default overrides here.
+    // Rename ASP.NET Core Identity table names, etc.
+  }
+}
--- Source/Fims.Server/Program.cs
+++ Source/Fims.Server/Program.cs
@@ -1,4 +1,23 @@
+using System.Security.Claims;
+using Fims.Server.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Identity
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+                       throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+  options.UseSqlite(connectionString));
+// builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+//
+// builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//   .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<ApplicationUser>()
+  .AddEntityFrameworkStores<ApplicationDbContext>();
 
 // Add services to the container.
 
@@ -11,6 +30,21 @@ var app = builder.Build();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
+
+// Identity API
+app.MapGroup("/api").MapIdentityApi<ApplicationUser>();
+app.MapPost("/api/logout", async (SignInManager<ApplicationUser> signInManager) =>
+{
+  await signInManager.SignOutAsync();
+  return Results.Ok();
+}).RequireAuthorization();
+app.MapGet("/api/pingauth", (ClaimsPrincipal user) =>
+{
+  // This is used by the frontend to acquire information about the logged in user,
+  // that's stored inside the HTTP-only cookie in the browser and not accessible from JS
+  var email = user.FindFirstValue(ClaimTypes.Email);
+  return Results.Json(new { Email = email });
+}).RequireAuthorization();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
