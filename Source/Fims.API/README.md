dcc61d8ed8bea0aa91a6128c8dad57460851d643
Markus <5563643+MarkusGeiger@users.noreply.github.com>
2024-10-03 01:13:24 +0200

    Add new backend API Integrate Keycloak for API authentication

---
+++ Source/Fims.API/README.md
@@ -0,0 +1,8 @@
+# Fims.API
+
+Attention: 
+1. Create Realm fims
+2. Create user aspire with Password Aspire
+3. Create client api-client, enable client authentication
+4. Edit client scopes for dedicated, add mapper from config, write api-client as audience into the access token
+
--- Source/Fims.AppHost/Program.cs
+++ Source/Fims.AppHost/Program.cs
@@ -1,3 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
+
+var keycloak = builder.AddKeycloakContainer("keycloak").WithDataVolume();
+
+var realm = keycloak.AddRealm("fims");
+
+builder.AddProject<Projects.Fims_API>("api")
+  .WithReference(keycloak)
+  .WithReference(realm);
+
+
 builder.AddProject<Projects.Fims_Server>("backend");
 builder.Build().Run();
